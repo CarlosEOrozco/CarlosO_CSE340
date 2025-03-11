2559e2ac6e877e41d3a61b1a7505bfea974e6a58
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-03-11 14:51:40 -0300

    update: cooments on assigment 2 and move 4 and 6 to rebuild the database

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,30 +1,29 @@
--- Insert a new record into the account table:
+-- 1. Insert a new record into the account table:
 INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
--- Modify the Tony Stark record to change the account_type to "Admin":
+-- 2. Modify the Tony Stark record to change the account_type to "Admin":
 UPDATE public.account
 SET account_type = 'Admin'
 WHERE account_email = 'tony@starkent.com';
 
--- Delete the Tony Stark record from the database:
+-- 3. Delete the Tony Stark record from the database:
 DELETE FROM public.account
 WHERE account_email = 'tony@starkent.com';
 
--- Modify the "GM Hummer" record to change "small interiors" to "a huge interior":
+-- 4. Modify the "GM Hummer" record to change "small interiors" to "a huge interior":
 UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
--- Use an inner join to select the make, model, and classification name for inventory items in the "Sport" category:
+-- 5. Use an inner join to select the make, model, and classification name for inventory items in the "Sport" category:
 SELECT inv.inv_make, inv.inv_model, cls.classification_name
 FROM public.inventory inv
 INNER JOIN public.classification cls ON inv.classification_id = cls.classification_id
 WHERE cls.classification_name = 'Sport';
 
--- Update all records in the inventory table to add "/vehicles" to the file paths in inv_image and inv_thumbnail:
+-- 6. Update all records in the inventory table to add "/vehicles" to the file paths in inv_image and inv_thumbnail:
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
 
-    
--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -237,3 +237,15 @@ VALUES   (
     'White',
     5
   );
+
+
+-- 4. Modify the "GM Hummer" record to change "small interiors" to "a huge interior":
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 6. Update all records in the inventory table to add "/vehicles" to the file paths in inv_image and inv_thumbnail:
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
