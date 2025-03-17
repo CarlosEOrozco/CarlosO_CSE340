19a6f919f9c7029b4ff39db0d564a64f4f8f60f6
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-03-17 16:26:39 -0300

    fix: possible fix

--- database/index.js
+++ database/index.js
@@ -1,42 +1,32 @@
-//allow multiple site visitors to be interacting with the database
-const { Pool } = require("pg")
-//allows the sensitive information about the database location 
-// and connection credentials to be stored in a separate location
-//  and still be accessed.
-require("dotenv").config()
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
-let pool
-// test to see if the code exists in a developent environment
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-})
+// database/index.js
+const { Pool } = require("pg");
+require("dotenv").config();
 
-// Added for troubleshooting queries
-// during development
+// Create a connection pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "development" ? { rejectUnauthorized: false } : false,
+});
+
+// Log executed queries for debugging
+pool.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+// Export a query function for use in the application
 module.exports = {
   async query(text, params) {
     try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
+      const res = await pool.query(text, params);
+      console.log("executed query", { text });
+      return res;
     } catch (error) {
-      console.error("error in query", { text })
-      throw error
+      console.error("error in query", { text });
+      throw error;
     }
   },
-}
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
-}
+};
