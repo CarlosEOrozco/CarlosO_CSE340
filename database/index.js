ada9239b5b0ef09ed86c91dcc8f575a613e273ca
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-03-17 17:10:24 -0300

    fis: ssl

--- database/index.js
+++ database/index.js
@@ -1,42 +1,35 @@
-//allow multiple site visitors to be interacting with the database
-const { Pool } = require("pg")
-//allows the sensitive information about the database location 
-// and connection credentials to be stored in a separate location
-//  and still be accessed.
-require("dotenv").config()
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
-let pool
-// test to see if the code exists in a developent environment
-if (process.env.NODE_ENV == "development") {
+const { Pool } = require("pg");
+require("dotenv").config();
+
+let pool;
+
+if (process.env.NODE_ENV === "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
+  });
+} else {
+  // Production environment
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // Add SSL for production
+    },
+  });
+}
 
-// Added for troubleshooting queries
-// during development
+// Added for troubleshooting queries during development
 module.exports = {
   async query(text, params) {
     try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
+      const res = await pool.query(text, params);
+      console.log("executed query", { text });
+      return res;
     } catch (error) {
-      console.error("error in query", { text })
-      throw error
+      console.error("error in query", { text });
+      throw error;
     }
   },
-}
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
-}
+};
