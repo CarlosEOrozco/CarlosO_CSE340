34c0d517ef67a65fa96f79fbc2f96e0f78632144
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-03-17 10:16:53 -0300

    update: SQL changes

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340;
+    OWNER TO test02; -- remember to put the DB name
 
 -- Table structure for table classification
 CREATE TABLE public.classification (
