ef53d3567dd4dcc82e033d295189d0c8862e277e
Carlos Orozco <carlosenmanuelorozcon@gmail.com>
2025-03-18 10:14:39 -0300

    update: error handler

--- controllers/invController.js
+++ controllers/invController.js
@@ -4,34 +4,42 @@ const utilities = require("../utilities/")
 const invCont = {}
 
 /* ***************************
- *  Build inventory by classification view
+ * Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
-}
+  try {
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    });
+  } catch (error) {
+    next(error); // Pass errors to the error-handling middleware
+  }
+};
 
 /* ***************************
- *  Build inventory item detail view
+ * Build inventory item detail view
  * ************************** */
 invCont.buildByInventoryId = async function (req, res, next) {
-  const inv_id = req.params.invId
-  const data = await invModel.getInventoryById(inv_id)
-  const detailHTML = await utilities.buildInventoryDetail(data)
-  let nav = await utilities.getNav()
-  res.render("./inventory/detail", {
-    title: data.inv_make + " " + data.inv_model,
-    nav,
-    detailHTML,
-  })
-}
+  try {
+    const inv_id = req.params.invId;
+    const data = await invModel.getInventoryById(inv_id);
+    const detailHTML = await utilities.buildInventoryDetail(data);
+    let nav = await utilities.getNav();
+    res.render("./inventory/detail", {
+      title: data.inv_make + " " + data.inv_model,
+      nav,
+      detailHTML,
+    });
+  } catch (error) {
+    next(error); // Pass errors to the error-handling middleware
+  }
+};
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,9 +4,9 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", Util.handleErrors(invController.buildByClassificationId));
 
 // Route to build inventory item detail view
-router.get("/detail/:invId", invController.buildByInventoryId);
+router.get("/detail/:invId", Util.handleErrors(invController.buildByInventoryId));
 
 module.exports = router;
