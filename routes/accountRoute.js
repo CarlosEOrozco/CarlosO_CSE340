c0cc944fcaf86a03fc80bb859fc9a4159739fe5d
412099_OROZCO <412099@tecnicatura.frc.utn.edu.ar>
2025-03-29 10:22:56 -0300

    update: account validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/accountController");
 const Util = require("../utilities");
+const regValidate = require('../utilities/account-validation');
 
 // GET Routes
 router.get("/login", Util.handleErrors(accountController.buildLogin));
@@ -11,8 +12,11 @@ router.get("/register", Util.handleErrors(accountController.buildRegister));
 /* ****************************************
 *  Process registration request
 * *************************************** */
-router.post("/register", 
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   Util.handleErrors(accountController.registerAccount)
-);
+)
 
 module.exports = router;
