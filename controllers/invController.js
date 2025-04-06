dfd68da6bc9974492a6717a6383e4f2719417964
412099-OROZCO <412099@tecnicatura.frc.utn.edu.ar>
2025-04-05 22:41:46 -0300

    controller function

--- controllers/invController.js
+++ controllers/invController.js
@@ -199,4 +199,17 @@ invCont.triggerError = async function (req, res, next) {
   throw new Error("Intentional 500 Error: Something went wrong!");
 };
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
+
 module.exports = invCont;
